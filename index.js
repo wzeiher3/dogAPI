6858869ffdf4496fd969c6ef6ade00b7bc2ed797
Will Zeiher <56980103+wzeiher3@users.noreply.github.com>
2020-08-17 12:29:59 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+//import $ from 'jquery';
+'use strict';
+let input = 3;
+function watchForm(){
+    $('form').submit(event=>{
+        event.preventDefault();
+        console.log($('.maxDogs').val())
+        if($('.maxDogs').val() == undefined)
+            input = 3;
+        else    
+            input = $('.maxDogs').val();
+        let url = `https://dog.ceo/api/breeds/image/random/${input}`;
+        console.log(url);
+        console.log(input);
+        getDogImage(url);
+    })
+    
+}
+
+function getDogImage(url){
+    console.log('getImage');
+    
+    fetch(url).then(function(response){
+        return response.json();
+    }).then(function(jsonData){
+        render(jsonData);
+        console.log(jsonData);
+    })
+}
+
+function render(data){
+    let htmlTemplate = [];
+    for(let i = 0; i < data.message.length; i++){
+        htmlTemplate.push(`
+            <img src="${data.message[i]}"></img>
+
+        `)
+    }
+    
+    console.log(htmlTemplate);
+    $('.dogPark').html(htmlTemplate);
+}
+
+$(function() {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+  });
